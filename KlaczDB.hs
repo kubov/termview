5f4de5fc6f4a4d6baa42161d67fe3a35d549fecc
kubov <kubov2@gmail.com>
2014-06-21 16:59:16 +0200

    queries

--- KlaczDB.hs
+++ KlaczDB.hs
@@ -5,12 +5,13 @@ import Database.HDBC.PostgreSQL
 
 conection = connectPostgreSQL "host=localhost user=klacz password=klacz"
 
-getTermsQuery = "select _name, count(*) from _entry join _term on (_term._oid = _entry._term_oid) group by _name order by _name;"
-getEntriesQuery = "select _added_by,_text from _entry join _term on (_term._oid = _entry._term_oid) where _name = ? order by _entry._oid DESC;"
+getTermsQuery = "select _name, count(*) from _entry join _term on (_term._oid = _entry._term_oid) where _term._visible = 't' and _entry._visible = 't' group by _name order by _name;"
 
-getMembersQuery = "select _added_by, count(*) as cc from _entry join _term on (_term._oid = _entry._term_oid) group by _added_by having count(*) > 0 order by _added_by;"
+getEntriesQuery = "select _added_by,_text from _entry join _term on (_term._oid = _entry._term_oid) where _entry._visible = 't' and _term._visible = 't' and _name = ? order by _entry._oid DESC;"
 
-getEntriesOfQuery = " select _name,_text from _entry join _term on (_term._oid = _entry._term_oid) where _added_by = ? order by _entry._oid DESC;"
+getMembersQuery = "select _added_by, count(*) as cc from _entry join _term on (_term._oid = _entry._term_oid) where _term._visible = 't' and _entry._visible = 't' group by _added_by having count(*) > 0 order by _added_by;"
+
+getEntriesOfQuery = "select _name,_text from _entry join _term on (_term._oid = _entry._term_oid) where  _term._visible = 't' and _entry._visible = 't' and _added_by = ? order by _entry._oid DESC;"
 
 queryPair :: String -> [SqlValue] -> IO [(String,String)]
 queryPair query params = do
