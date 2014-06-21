e85ba1f038fa1d90b220e98818e36bcaaea26ecc
kubov <kubov2@gmail.com>
2014-06-21 13:04:35 +0200

    members submissons

--- KlaczDB.hs
+++ KlaczDB.hs
@@ -3,22 +3,30 @@ module KlaczDB where
 import Database.HDBC
 import Database.HDBC.PostgreSQL
 
-conection = connectPostgreSQL "host=localhost user=klacz"
+conection = connectPostgreSQL "host=localhost user=klacz password=klacz"
 
-getTermsQuery = "select _name, count(*) from _entry join _term on (_term._oid = _entry._term_oid) group by _name;"
+getTermsQuery = "select _name, count(*) from _entry join _term on (_term._oid = _entry._term_oid) group by _name order by _name;"
 getEntriesQuery = "select _added_by,_text from _entry join _term on (_term._oid = _entry._term_oid) where _name = ? order by _entry._oid DESC;"
 
-getTerms :: IO [(String,Int)]
-getTerms  = do
+getMembersQuery = "select _added_by, count(*) as cc from _entry join _term on (_term._oid = _entry._term_oid) group by _added_by having count(*) > 0 order by _added_by;"
+
+getEntriesOfQuery = " select _name,_text from _entry join _term on (_term._oid = _entry._term_oid) where _added_by = ? order by _entry._oid DESC;"
+
+queryPair :: String -> [SqlValue] -> IO [(String,String)]
+queryPair query params = do
   con <- conection
-  vals <- quickQuery' con getTermsQuery []
+  vals <- quickQuery' con query params
   return (map unwrap vals) where
-    unwrap [desc, count] = (fromSql desc, fromSql count)
-      
+    unwrap [f, s] = (fromSql f, fromSql s)
+
+getTerms :: IO [(String,String)]
+getTerms = queryPair getTermsQuery []
       
 getEntries :: String -> IO [(String, String)]
-getEntries term = do
-  con <- conection
-  vals <- quickQuery' con getEntriesQuery [toSql term]
-  return (map unwrap vals) where
-    unwrap [by, text] = (fromSql by, fromSql text)
+getEntries term = queryPair getEntriesQuery [toSql term]
+
+getMembers :: IO [(String,String)]
+getMembers = queryPair getMembersQuery []
+
+getEntriesOf :: String -> IO [(String, String)]
+getEntriesOf term = queryPair getEntriesOfQuery [toSql term]
--- Main.hs
+++ Main.hs
@@ -4,17 +4,71 @@
 
 module Main where
 import Yesod
+import KlaczDB
 
 data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
+/term/#String TermR GET
+/members/ MembersR GET
+/entriesof/#String EntriesOfR GET
 |]
 
 instance Yesod HelloWorld
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = do
+  terms <- liftIO $ getTerms
+  defaultLayout [whamlet|
+                 <h2>termview
+                 <h4><a href="@{MembersR}">check entries of members
+                 <ul>
+                   $forall (name,count) <- terms
+                           <li><a href="@{TermR name}">#{name} (#{count})
+                 |]
+
+getTermR :: String -> Handler Html
+getTermR term = do
+  entires <- liftIO $ getEntries term
+  defaultLayout [whamlet|
+                 <h3>#{term}
+
+                 <table>
+                   <tr>
+                   $forall (by,text) <- entires
+                     <tr>
+                       <td>#{by}
+                       <td><b>#{text}
+
+                 <h4><a href="@{HomeR}">home
+               |]
+
+getMembersR :: Handler Html
+getMembersR = do
+  members <- liftIO getMembers
+  defaultLayout [whamlet|
+                 <h2>Entries of
+                 <h4><a href="@{HomeR}">home
+                 <ul>
+                   $forall (name,count) <- members
+                           <li><a href="@{EntriesOfR name}">#{name} (#{count})
+                 |]
+
+getEntriesOfR :: String -> Handler Html
+getEntriesOfR term = do
+  entires <- liftIO $ getEntriesOf term
+  defaultLayout [whamlet|
+                 <h3>#{term} added
+                 <table>
+                   <tr>
+                   $forall (by,text) <- entires
+                     <tr>
+                       <td>#{by}
+                       <td><b>#{text}
+
+                 <h4><a href="@{MembersR}">home
+               |]
 
 main :: IO ()
 main = warp 3000 HelloWorld
