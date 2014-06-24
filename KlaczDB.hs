e293cc3e7c8879fa484ce53606c559959245708c
kubov <kubov2@gmail.com>
2014-06-24 12:43:06 +0200

    ordering

--- KlaczDB.hs
+++ KlaczDB.hs
@@ -5,11 +5,11 @@ import Database.HDBC.PostgreSQL
 
 conection = connectPostgreSQL "host=localhost user=klacz password=klacz"
 
-getTermsQuery = "select _name, count(*) from _entry join _term on (_term._oid = _entry._term_oid) where _term._visible = 't' and _entry._visible = 't' group by _name order by _name;"
+getTermsQuery = "select _name, count(*) from _entry join _term on (_term._oid = _entry._term_oid) where _term._visible = 't' and _entry._visible = 't' group by _name order by upper(_name);"
 
 getEntriesQuery = "select _added_by,_text from _entry join _term on (_term._oid = _entry._term_oid) where _entry._visible = 't' and _term._visible = 't' and _name = ? order by _entry._oid DESC;"
 
-getMembersQuery = "select _added_by, count(*) as cc from _entry join _term on (_term._oid = _entry._term_oid) where _term._visible = 't' and _entry._visible = 't' group by _added_by having count(*) > 0 order by _added_by;"
+getMembersQuery = "select _added_by, count(*) as cc from _entry join _term on (_term._oid = _entry._term_oid) where _term._visible = 't' and _entry._visible = 't' group by _added_by having count(*) > 0 order by upper(_added_by);"
 
 getEntriesOfQuery = "select _name,_text from _entry join _term on (_term._oid = _entry._term_oid) where  _term._visible = 't' and _entry._visible = 't' and _added_by = ? order by _entry._oid DESC;"
 
